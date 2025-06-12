258cd3c9cdad9c810775b1a0a88f7af145a893ed
Jason Eagleston <jasoneaglestonjr@outlook.com>
2025-06-12 11:22:45 -0400

    Map updating from server.

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,6 +5,7 @@ extends Node2D
 var socket = WebSocketPeer.new()
 
 var clients: Array = []
+var maps: Array = []
 
 var is_connecting: bool = false
 
@@ -13,10 +14,13 @@ func try_connect(url):
 	var err = socket.connect_to_url(url)
 	if err != OK:
 		print("Unable to connect.")
+		
+func load_creation_menu():
+	$CreationMenu.show()
+		
 func connected():
-	add_child(preload("res://scenes/demo.tscn").instantiate())
-func _ready():
-	try_connect(websocket_url)
+	load_creation_menu()
+	$MainMenu.hide()
 
 enum PacketType {
 	LOGIN = 1,
@@ -44,23 +48,34 @@ func get_string(buf: PackedByteArray, pos: int) -> String:
 	var size = buf[pos]
 	var s = buf.slice(pos + 1, pos + 1 + size).get_string_from_utf8()
 	return s
+
 func load_maps(buf: PackedByteArray, pos: int):
-	# [STRING LENGTH, STRING, MAP SIZE u16, [TILE_ID u16...MAP_SIZE], REPEAT?]
+	# [STRING LENGTH, STRING, MAP WIDTH u16, MAP HEIGHT u16, [TILE_ID u16...MAP_SIZE], REPEAT?]
+	var tiles = []
 	var map_name = get_string(buf, pos)
 	pos += 1 + buf[pos]
-	var map_size = buf.decode_u16(pos)
+	var map_width = buf.decode_u16(pos)
 	pos += 2
-	for i in map_size:
-		var tile_id = buf.decode_u16(pos)
+	var map_height = buf.decode_u16(pos)
+	pos += 2
+	for i in map_width * map_height:
+		tiles.append(buf.decode_u16(pos))
 		pos += 2
+	var map = load("res://scenes/map.tscn").instantiate()
+	map.hide()
+	get_tree().get_root().add_child(map)
+	map.load_map(map_name, map_width, map_height, tiles)
+	maps.append(map)
 	if pos != buf.size():
 		load_maps(buf, pos)
 
 func get_races(buf: PackedByteArray):
 	# [RACE LIST LENGTH, STRING LENGTH, RACE NAME STRING, REPEAT?]
 	var len = buf[1]
+	var available_races = []
 	for s in get_string_mult(buf, 2):
-		print(s)
+		available_races.append(s)
+	$CreationMenu.set_races(available_races)
 	
 
 func add_client(data: PackedByteArray):
@@ -75,7 +90,7 @@ func sync_clients(data: PackedByteArray):
 		print("CLIENT: ", client_id)
 		clients.append(client_id)
 		pos += 8
-		
+			
 func send_move_vec():
 	var data: PackedByteArray = [PacketType.CLIENT_MOVE_REQUEST, move_vec.x && 0xFF, move_vec.y && 0xFF]
 	socket.send(data)
@@ -134,3 +149,7 @@ func _process(dt):
 		send_move_vec()
 				
 	old_move_vec = move_vec
+
+
+func _on_connect_pressed() -> void:
+	try_connect(websocket_url)
